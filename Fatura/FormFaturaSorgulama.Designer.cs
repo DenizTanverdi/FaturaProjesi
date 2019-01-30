906728473c3b41b3306eef355dc27e2567747b81
semvs <semvs@ayvansaray.edu.tr>
2019-01-30 11:25:45 +0300

    FaturaDuzenle

---
+++ Fatura/FormFaturaSorgulama.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Fatura
+{
+    partial class FormFaturaSorgulama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboSehir = new System.Windows.Forms.ComboBox();
+            this.comboİlce = new System.Windows.Forms.ComboBox();
+            this.comboMusteri = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBxFaturaNo = new System.Windows.Forms.TextBox();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboSehir
+            // 
+            this.comboSehir.FormattingEnabled = true;
+            this.comboSehir.Location = new System.Drawing.Point(341, 43);
+            this.comboSehir.Name = "comboSehir";
+            this.comboSehir.Size = new System.Drawing.Size(121, 21);
+            this.comboSehir.TabIndex = 0;
+            // 
+            // comboİlce
+            // 
+            this.comboİlce.FormattingEnabled = true;
+            this.comboİlce.Location = new System.Drawing.Point(341, 82);
+            this.comboİlce.Name = "comboİlce";
+            this.comboİlce.Size = new System.Drawing.Size(121, 21);
+            this.comboİlce.TabIndex = 1;
+            // 
+            // comboMusteri
+            // 
+            this.comboMusteri.FormattingEnabled = true;
+            this.comboMusteri.Location = new System.Drawing.Point(341, 123);
+            this.comboMusteri.Name = "comboMusteri";
+            this.comboMusteri.Size = new System.Drawing.Size(121, 21);
+            this.comboMusteri.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Şehir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "İlçe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Müşteri Adı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fatura No";
+            // 
+            // txtBxFaturaNo
+            // 
+            this.txtBxFaturaNo.Location = new System.Drawing.Point(72, 79);
+            this.txtBxFaturaNo.Name = "txtBxFaturaNo";
+            this.txtBxFaturaNo.Size = new System.Drawing.Size(100, 20);
+            this.txtBxFaturaNo.TabIndex = 4;
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(533, 43);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(115, 101);
+            this.btnGetir.TabIndex = 5;
+            this.btnGetir.Text = "Tüm Siparişleri Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(649, 247);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Tag = "lama";
+            this.label5.Text = "Fatura No İle Sorgulama";
+            // 
+            // FormFaturaSorgulama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(649, 471);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.txtBxFaturaNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboMusteri);
+            this.Controls.Add(this.comboİlce);
+            this.Controls.Add(this.comboSehir);
+            this.Name = "FormFaturaSorgulama";
+            this.Text = "FormFaturaSorgulama";
+            this.Load += new System.EventHandler(this.FormFaturaSorgulama_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboSehir;
+        private System.Windows.Forms.ComboBox comboİlce;
+        private System.Windows.Forms.ComboBox comboMusteri;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBxFaturaNo;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Fatura/FormFaturaSorgulama.cs
@@ -0,0 +1,27 @@
+﻿using Fatura.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fatura
+{
+    public partial class FormFaturaSorgulama : Form
+    {
+        FaturaContext db = new FaturaContext();
+        public FormFaturaSorgulama()
+        {
+            InitializeComponent();
+        }
+
+        private void FormFaturaSorgulama_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
